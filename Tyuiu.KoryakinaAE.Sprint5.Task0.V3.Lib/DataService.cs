0dfc6ac712b3fcc94f45869010882a35c3213989
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-11-23 21:05:51 +0500

    Task0

--- Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib/DataService.cs
+++ Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             double y = -(1.0 / 4) * (Math.Pow(x, 3) - 3 * Math.Pow(x, 2) + 4);
             File.WriteAllText(path, y.ToString());
             return path;
