4eee0cfacfb2a65d6a3941e12a75d58f30350244
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-11-23 20:54:41 +0500

    Task0

---
+++ Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib
+{
+    public class DataService : ISprint5Task0V3
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double y = -(1.0 / 4) * (Math.Pow(x, 3) - 3 * Math.Pow(x, 2) + 4);
+            File.WriteAllText(path, y.ToString());
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint5.Task0.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = $@"C:\Users\korya\source\repos\Tyuiu.KoryakinaAE.Sprint5\Tyuiu.KoryakinaAE.Sprint5.Task0.V3\bin\Debug\net8.0\OutPutFileTask0.txt";
+            bool result = File.Exists(path);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
