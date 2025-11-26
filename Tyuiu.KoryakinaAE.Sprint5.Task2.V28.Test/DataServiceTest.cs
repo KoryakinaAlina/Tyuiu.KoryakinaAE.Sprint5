f264c31b44873eb782c29acd013a422382cd2071
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-11-27 01:31:27 +0500

    Task2

---
+++ Tyuiu.KoryakinaAE.Sprint5.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KoryakinaAE.Sprint5.Task2.V28.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint5.Task2.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3];
+            string path = ds.SaveToFileTextData(mtrx);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KoryakinaAE.Sprint5.Task2.V28/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KoryakinaAE.Sprint5.Task2.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mtrx = new int[3, 3] { { -5, -2, -5 },
+                              { 3, 1, -3 },
+                              { 9, -2, -9 } };
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Корякина А. Э. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл.     *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнила: Корякина Алина Эльдаяговна | ИСТНб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(mtrx);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
