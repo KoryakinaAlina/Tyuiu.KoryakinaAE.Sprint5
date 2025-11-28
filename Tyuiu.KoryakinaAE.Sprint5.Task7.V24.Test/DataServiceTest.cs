d88362a3ad55a4d4db091b974a9d9fe343b982fc
KoryakinaAlina <Tyuiu.KoryakinaAE@outlook.com>
2025-11-28 15:40:22 +0500

    Task7

---
+++ Tyuiu.KoryakinaAE.Sprint5.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KoryakinaAE.Sprint5.Task7.V24.Lib;
+
+namespace Tyuiu.KoryakinaAE.Sprint5.Task7.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KoryakinaAE.Sprint5.Task7.V24/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Tyuiu.KoryakinaAE.Sprint5.Task7.V24.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Корякина Алина Эльдаяговна| ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Чтение набора данных из текстового файла                           *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Корякина Алина Эльдаяговна | ИСТНб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Заменить все русские слова на слово - слово  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+        string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V24.txt");
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Находится в файле: ");
+        pathSaveFile = ds.LoadDataAndSave(path);
+        Console.WriteLine(pathSaveFile);
+
+        Console.ReadKey();
+    }
+}
